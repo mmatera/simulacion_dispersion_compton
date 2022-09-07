8b7897d771fe4a7c4c62f8a01b79003b8a3cb799
mmatera <matera@fisica.unlp.edu.ar>
2022-09-07 19:26:11 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+requires = ['matplotlib',
+            'numpy',
+            ]
+
+setup(name='compton_scattering',
+      version='0.1',
+      install_requires=requires,
+      description=('Simulación de un experimento de'
+                   'dispersión Compton en correlaciones'),
+      author="Mauricio Matera",
+      author_email='matera@fisica.unlp.edu.ar',
+      py_modules=['compton_scattering'],
+      )
